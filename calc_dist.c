a880c6c174afa224b1bb36b302ea8ff0222d6f4c
Dibyo Majumdar <dibyo.majumdar@gmail.com>
2014-04-10 01:34:21 -0700

    Updated calc_dist

--- calc_dist.c
+++ calc_dist.c
@@ -16,50 +16,50 @@
 // #include <signal.h>
 
 /* Find the squared difference between two pixels */
-unsigned int squared_distance(unsigned char d1, unsigned char d2) {
-	unsigned int distance;
-    distance = d1 - d2;
+float squared_distance(float d1, float d2) {
+	float distance;
+  distance = d1 - d2;
 	return (distance * distance);
 }
 
 /* Swaps the values pointed to by the pointers X and Y. */
-void swap_char(unsigned char *x, unsigned char *y) {
-    unsigned char temp;
-    temp = *x;
-    *x = *y;
-    *y = temp;
+void swap_char(float *x, float *y) {
+  float temp;
+  temp = *x;
+  *x = *y;
+  *y = temp;
 }
 
 /* Swaps the values pointed to by the pointers X and Y. */
 void swap_int(int *x, int *y) {
-    int temp;
-    temp = *x;
-    *x = *y;
-    *y = temp;
+  int temp;
+  temp = *x;
+  *x = *y;
+  *y = temp;
 }
 
 /* Flips the elements of a square array ARR across the y-axis. */
-void flip_horizontal(unsigned char *arr, int width) {
-    for (int i = 0; i < width; i++) {
-    	for (int j = 0; j < (width / 2); j++) {
-    		swap_char(arr + (width)*i + j, arr + (width)*i + (width) - j - 1);
-   		}
-    }
+void flip_horizontal(float *arr, int width) {
+  for (int i = 0; i < width; i++) {
+  	for (int j = 0; j < (width / 2); j++) {
+  		swap_char(arr + (width)*i + j, arr + (width)*i + (width) - j - 1);
+ 		}
+  }
 }
 
 /* Transposes the square array ARR. */
-void transpose(unsigned char *arr, int width) {
-    unsigned char *result;
-    result = (unsigned char*) malloc(sizeof(unsigned char) * width * width);
-    for (int i = 0; i < width; i++) {
-    	for (int j = 0; j < width; j++) {
-    		result[i + j * width] = arr[i * width + j];
-    	}
-    }
-    for (int index = 0; index < (width * width); index++) {
-      arr[index] = result[index];
-    }
-    free(result);
+void transpose(float *arr, int width) {
+  float *result;
+  result = (float*) malloc(sizeof(float) * width * width);
+  for (int i = 0; i < width; i++) {
+  	for (int j = 0; j < width; j++) {
+  		result[i + j * width] = arr[i * width + j];
+  	}
+  }
+  for (int index = 0; index < (width * width); index++) {
+    arr[index] = result[index];
+  }
+  free(result);
 }
 
 /* Rotates the square array ARR by 90 degrees counterclockwise. */
@@ -70,14 +70,10 @@ void rotate_ccw_90(unsigned char *arr, int width) {
 /* Find sum of squares pixel to pixel of two square images of the same size and
  * compare to existing sum.
  */
-unsigned int least_sum_squares(unsigned char *i1, unsigned char *i2,
-        int width, unsigned int *least_sum) {
-    unsigned int sum = 0, distance;
+void least_sum_squares(float *i1, float *i2, int width,
+                        float *least_sum) {
+    float sum = 0.0;
     for (int i = 0; i < (width*width); i++) {
-        
-        distance = (unsigned int) squared_distance(i1[i], i2[i]);
-        if (distance > 0) {
-        } 
         sum += squared_distance(i1[i], i2[i]);
     }
     if (sum < *least_sum) {
@@ -85,8 +81,8 @@ unsigned int least_sum_squares(unsigned char *i1, unsigned char *i2,
     }
 }
 
-void extract_portion(unsigned char *portion, unsigned char *image,
-        int i, int j, int t_width, int i_width) {
+void extract_portion(float *portion, float *image, int i, int j,
+                     int t_width, int i_width) {
 	for (int x = 0; x < t_width; x++) {
 		for (int y = 0; y < t_width; y++) {
 			portion[x + y*t_width] = image[(i + x) + (j + y)*i_width];
@@ -98,20 +94,20 @@ void extract_portion(unsigned char *portion, unsigned char *image,
  * is I_WIDTH * I_HEIGHT, while TEMPLATE is square with side length T_WIDTH. The template
  * image should be flipped, rotated, and translated across IMAGE.
  */
-unsigned int calc_min_dist(unsigned char *image, int i_width, int i_height, 
-        unsigned char *template, int t_width) {
-    unsigned int min_dist = UINT_MAX;
-    unsigned char * portion;
-    portion = (unsigned char*) malloc(sizeof(unsigned char)* t_width * t_width);
+float calc_min_dist(float *image, int i_width, int i_height, 
+                    float *template, int t_width) {
+  float min_dist = UINT_MAX;
+  float * portion;
+  portion = (float*) malloc(sizeof(float)* t_width * t_width);
 	for (int i = 0; i <= (i_width - t_width); i++) {
 		for (int j = 0; j <= (i_height - t_width); j++) {
 			extract_portion(portion, image, i, j, t_width, i_width);
-            for (int counter = 0; counter < 4; counter++) {
-    			least_sum_squares(portion, template, t_width, &min_dist);
-    			flip_horizontal(portion, t_width);
-    			least_sum_squares(portion, template, t_width, &min_dist);
-    			transpose(portion, t_width);
-            }
+      for (int counter = 0; counter < 4; counter++) {
+  			least_sum_squares(portion, template, t_width, &min_dist);
+  			flip_horizontal(portion, t_width);
+  			least_sum_squares(portion, template, t_width, &min_dist);
+  			transpose(portion, t_width);
+      }
 		}
 	}
 	return min_dist;
