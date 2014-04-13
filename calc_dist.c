2bfa6401e6cf2c459474e17127adf1c6d18d530a
Dibyo Majumdar <dibyo.majumdar@gmail.com>
2014-04-13 06:50:58 -0700

    27 Gflops/s

--- calc_dist.c
+++ calc_dist.c
@@ -154,11 +154,11 @@ void least_sum_squares(float *image, float *template, int i_width, int t_width,
                         float *least_sum, int offx, int offy) {
   float sum1 = 0.0, sum2 = 0.0, sum3 = 0.0, sum4 = 0.0,
         sum5 = 0.0, sum6 = 0.0, sum7 = 0.0, sum8 = 0.0;
-  float v[4];
-  __m128 sum1_vector, sum2_vector, a, b;
-  sum1_vector = _mm_setzero_ps();
   int offset = offx + i_width*offy;
-  int total_pix = t_width*t_width - 1;
+  int total_pix = t_width*t_width - 1; /*
+  float v[4], *pin;
+  __m128 sum1_vector, sum2_vector, sum4_vector, a, b;
+  sum1_vector = _mm_setzero_ps();
   for (int i = 0; i < t_width; i++) {
     for (int j = 0; j < (t_width/4)*4; j+=4) {
       // normal
@@ -166,11 +166,13 @@ void least_sum_squares(float *image, float *template, int i_width, int t_width,
       b = _mm_loadu_ps(template + i*t_width + j);
       sum1_vector = _mm_add_ps(sum1_vector, squared_distance_vector(a, b));
       // normal flip
-      // sum2 += squared_distance(image[offset + i*i_width + j], template[total_pix - i*t_width - j]);
-      // sum2 += squared_distance(image[offset + i*i_width + j+1], template[total_pix - i*t_width - (j+1)]);
-      // sum2 += squared_distance(image[offset + i*i_width + j+2], template[total_pix - i*t_width - (j+2)]);
-      // sum2 += squared_distance(image[offset + i*i_width + j+3], template[total_pix - i*t_width - (j+3)]);
-      b = _mm_loadr_ps(template + total_pix - i*t_width - (j+3));
+      sum2 += squared_distance(image[offset + i*i_width + j], template[total_pix - i*t_width - j]);
+      sum2 += squared_distance(image[offset + i*i_width + j+1], template[total_pix - i*t_width - (j+1)]);
+      sum2 += squared_distance(image[offset + i*i_width + j+2], template[total_pix - i*t_width - (j+2)]);
+      sum2 += squared_distance(image[offset + i*i_width + j+3], template[total_pix - i*t_width - (j+3)]);
+      pin = template + total_pix - i*t_width - j;
+      // b = _mm_loadr_ps(template + total_pix - i*t_width - j - 3);
+      b = _mm_set_ps(*pin, *(pin-1), *(pin-2), *(pin-3));
       sum2_vector = _mm_add_ps(sum2_vector, squared_distance_vector(a, b));
       // reverse
       sum3 += squared_distance(image[offset + i*i_width + j], template[(i+1)*t_width - j-1]);
@@ -182,6 +184,8 @@ void least_sum_squares(float *image, float *template, int i_width, int t_width,
       sum4 += squared_distance(image[offset + i*i_width + j+1], template[total_pix - (i+1)*t_width + j+2]);
       sum4 += squared_distance(image[offset + i*i_width + j+2], template[total_pix - (i+1)*t_width + j+3]);
       sum4 += squared_distance(image[offset + i*i_width + j+3], template[total_pix - (i+1)*t_width + j+4]);
+      b = _mm_loadu_ps(template + total_pix - (i+1)*t_width + j+1);
+      sum4_vector = _mm_add_ps(sum4_vector, squared_distance_vector(a, b));
     }
   }
   _mm_storeu_ps(v, sum1_vector);
@@ -191,7 +195,11 @@ void least_sum_squares(float *image, float *template, int i_width, int t_width,
   _mm_storeu_ps(v, sum2_vector);
   for (int p = 0; p < 4; p++) {
     sum2 += *(v + p);
-  } 
+  }
+  _mm_storeu_ps(v, sum4_vector);
+  for (int p = 0; p < 4; p++) {
+    sum4 += *(v + p);
+  }
   if (t_width % 4 != 0) {
     for (int i = 0; i < t_width; i++) {
       for (int j = (t_width/4)*4; j < t_width; j++) {
@@ -201,7 +209,7 @@ void least_sum_squares(float *image, float *template, int i_width, int t_width,
         sum4 += squared_distance(image[offset + i*i_width + j], template[total_pix - (i+1)*t_width + j+1]);
       }
     }
-  } /*
+  } */
   for (int i = 0; i < t_width; i++) {
     for (int j = 0; j < t_width; j++) {
       sum1 += squared_distance(image[offset + i*i_width + j], template[i*t_width + j]);
@@ -209,7 +217,7 @@ void least_sum_squares(float *image, float *template, int i_width, int t_width,
       sum3 += squared_distance(image[offset + i*i_width + j], template[(i+1)*t_width - j-1]);
       sum4 += squared_distance(image[offset + i*i_width + j], template[total_pix - (i+1)*t_width + j+1]);
     }
-  } */
+  }
   for (int j = 0; j < t_width; j++) {
     for (int i = 0; i < t_width; i++) {
        // normal transpose
@@ -277,9 +285,20 @@ float calc_min_dist(float *image, int i_width, int i_height,
                     float *template, int t_width) {
   float min_dist = UINT_MAX;
   // float portion[t_width*t_width];
-  for (int i = 0; i <= (i_width - t_width); i++) {
+  #pragma omp parallel
+  {
+    float dist = UINT_MAX;
+    #pragma omp for collapse(2)
     for (int j = 0; j <= (i_height - t_width); j++) {
-      least_sum_squares(image, template, i_width, t_width, &min_dist, i, j);
+      for (int i = 0; i <= (i_width - t_width); i++) {
+        least_sum_squares(image, template, i_width, t_width, &dist, i, j);
+      }
+    }
+    #pragma omp critical
+    {
+      if (dist < min_dist) {
+        min_dist = dist;
+      }
     }
   }
   return min_dist;
